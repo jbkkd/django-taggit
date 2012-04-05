525f88404eec36a02dcad03d0eca81e231637b06
Lorenzo Franceschini <lorenzo.franceschini@informaetica.it>
2012-04-05 19:32:53 +0200

    Major improvements.

--- taggit/templatetags/taggit_tags.py
+++ taggit/templatetags/taggit_tags.py
@@ -2,13 +2,16 @@ from django import template
 from django.db import models
 from django.db.models import Count
 from django.db.models.loading import get_model
-from django.core.exceptions import FieldError, ImproperlyConfigured
+from django.db.models.query import QuerySet
+from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 
 from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 
-from templatetag_sugar.register import tag
-from templatetag_sugar.parser import Name, Variable, Constant, Optional, Model
+from generic_aggregation import generic_annotate
+
+import re
 
 ## retrieve configuration options
 ## if a config option is undefined, set a sane default value
@@ -33,7 +36,7 @@ def is_generic_tagging(through_model):
     raises an ``ImproperlyConfigured`` exception.
     
     
-    .. [*] i.e. the Django model which represents a tagged item w.r.t. a given tagging model
+    .. [*] i.e. the Django model which represents a tagged item w.r.t. a given tagging model.
     """
     opts = through_model._meta
     err_msg = "The object %s doesn't seem to be a valid through model for tagging" % through_model
@@ -41,7 +44,7 @@ def is_generic_tagging(through_model):
     if 'content_object' in opts.get_all_field_names():
         field = opts.get_field_by_name('content_object')[0]
         if isinstance(field, models.ForeignKey):
-            # the ``content_object`` attribute of the through model is a FK.
+            # the ``content_object`` attribute of the through model is a FK,
             # so we have a model-specific tagging model 
             return False
         else:
@@ -49,137 +52,144 @@ def is_generic_tagging(through_model):
     else:
         # search for GFKs 
         # since a GFK is a dummy field  -- it doesn't map to an actual DB column --
-        # it's not listed among "genuine" model fields, but among so called "virtual fields"
+        # it's not listed together with "genuine" model fields, but among so-called "virtual fields"
         for field in opts.virtual_fields:       
             if isinstance(field, generic.GenericForeignKey) and field.name == 'content_object': 
                 return True
         raise ImproperlyConfigured(err_msg)
-    
 
-def get_tag_queryset(forvar=None):
-    """
-    WRITEME
-    """
-    # Django model representing tags
-    tag_model = get_model(*TAG_MODEL.split('.'))
-    # Django model representing tagged items (w.r.t. the tagging model in use)
-    through_model = get_model(*TAGGED_ITEM_MODEL.split('.'))
-    
-    count_field = None
-
-    if forvar is None:
-        # get all tags
-        qs = TAG_MODEL._default_manager.all()
-    else:
-        # extract app label and model name
-        appl_abel, model = None, None, None
-        try:
-            beginning, applabel, model = forvar.rsplit('.', 2)
-        except ValueError:
-            try:
-                applabel, model = forvar.rsplit('.', 1)
-            except ValueError:
-                applabel = forvar
-        applabel = applabel.lower()
-        
-        # filter tagged items        
-        if model is None:
-            # Get tags for a whole app
-            qs = TAGGED_ITEM_MODEL._default_manager.filter(content_type__app_label=app_label)
-            tag_ids = qs.values_list('tag_id', flat=True)
-            qs = TAG_MODEL._default_manager.filter(id__in=tag_ids)
-        else:
-            # Get tags for a model
-            model = model.lower()
-            if ":" in model:
-                model, manager_attr = model.split(":", 1)
-            else:
-                manager_attr = "tags"
-            model_class = get_model(applabel, model)
-            manager = getattr(model_class, manager_attr)
-            queryset = manager.all()
-            through_opts = manager.through._meta
-            count_field = ("%s_%s_items" % (through_opts.app_label,
-                    through_opts.object_name)).lower()
-
-    if count_field is None:
-        # Retain compatibility with older versions of Django taggit
-        # a version check (for example taggit.VERSION <= (0,8,0)) does NOT
-        # work because of the version (0,8,0) of the current dev version of django-taggit
-        try:
-            return queryset.annotate(num_times=Count(settings.TAG_FIELD_RELATED_NAME))
-        except FieldError:
-            return queryset.annotate(num_times=Count('taggit_taggeditem_items'))
-    else:
-        return queryset.annotate(num_times=Count(count_field))
-
-
-def get_weight_fun(t_min, t_max, f_min, f_max):
-    def weight_fun(f_i, t_min=t_min, t_max=t_max, f_min=f_min, f_max=f_max):
-        # Prevent a division by zero here, found to occur under some
-        # pathological but nevertheless actually occurring circumstances.
-        if f_max == f_min:
-            mult_fac = 1.0
-        else:
-            mult_fac = float(t_max-t_min)/float(f_max-f_min)
-        return t_max - (f_max-f_i)*mult_fac
-    return weight_fun
-
-@tag(register,[Constant('as'), Name(), 
-               Optional([Constant('for'), Variable()]), 
-               Optional([Constant('limit'), Variable()])
-               ])
-
-def get_taglist(context, asvar, forvar=None, limit=10):
-    queryset = get_queryset(forvar)         
-    queryset = queryset.order_by('-num_times')        
-    context[asvar] = queryset
-    if limit:
-        queryset = queryset[:limit]
-    return ''
-
-@tag(register, [Constant('as'), Name(), Optional([Constant('for'), Variable()]), Optional([Constant('limit'), Variable()]),])
-def get_tagcloud(context, asvar, forvar=None, limit=None):
-    queryset = get_queryset(forvar)
-    num_times = queryset.values_list('num_times', flat=True)
-    if(len(num_times) == 0):
-        context[asvar] = queryset
-        return ''
-    weight_fun = get_weight_fun(T_MIN, T_MAX, min(num_times), max(num_times))
-    queryset = queryset.order_by('name')
-    if limit:
-        queryset = queryset[:limit]
-    for tag in queryset:
-        tag.weight = weight_fun(tag.num_times)
-    context[asvar] = queryset
-    return ''
  
-# method from
-# https://github.com/dokterbob/django-taggit-templatetags/commit/fe893ac1c93d58cd122c621804f311430c93dc12  
-# {% get_similar_obects to product as similar_videos for metaphore.embeddedvideo %}
-@tag(register, [Constant('to'), Variable(), Constant('as'), Name(), Optional([Constant('for'), Model()])])
-def get_similar_objects(context, tovar, asvar, forvar=None):
-    if forvar:
-        assert hasattr(tovar, 'tags')
-        tags = tovar.tags.all()
-        from django.contrib.contenttypes.models import ContentType
-        ct = ContentType.objects.get_for_model(forvar)
-        items = TaggedItem.objects.filter(content_type=ct, tag__in=tags)
-        from django.db.models import Count
-        ordered = items.values('object_id').annotate(Count('object_id')).order_by()
-        ordered_ids = map(lambda x: x['object_id'], ordered)
-        objects = ct.model_class().objects.filter(pk__in=ordered_ids)
-    else:
-        objects = tovar.tags.similar_objects()
-    context[asvar] = objects    
-    return ''    
-
     
-def include_tagcloud(forvar=None):
-    return {'forvar': forvar}
+@register.tag(name="get_tag_list")
+def do_get_tag_list(parser, token):
+    try:
+        # Splitting by ``None`` == splitting by spaces
+        tag_name, args = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("'%r' tag requires at least an 'as' clause" % token.contents.split()[0])
+    tag_signature = r'''^\s*                                # match the start of the string 
+                     (?P<for_clause>for\s+[.'"\w]+)?\s+     # optional 'for' clause
+                     (?P<limit_clause>limit\s+\d+)?\s+      # optional 'limit' clause
+                     (?P<as_clause>as\s+\w+)\s*             # mandatory 'as' clause
+                     $                                      # match the end of the string
+                     '''
+    tag_signature = re.compile(tag_signature, re.VERBOSE)
+    m = re.search(tag_signature, args)
+    if not m: # bad syntax for tag arguments 
+        raise template.TemplateSyntaxError("%s tag has been called with invalid arguments: %s" % tag_name, args)
+    bits = m.groupdict() # dictionary of matched substrings, keyed by group names
+    
+    forvar = bits['for_clause'].split()[1]        
+    limitvar = bits['limit_clause'].split()[1]
+    asvar = bits['as_clause'].split()[1]
+    
+    app_label, model_name, content_qs_var, limit = (None,) * 4 
+    
+    if forvar:    
+        if forvar[0] == forvar[-1] and forvar[0] in ('"', "'"): # quote-enclosed arg
+            forvar = forvar[1:-1] # strip quotes from arg
+            # since ``for`` clause's argument is quoted, we intepret it 
+            # as a string of the form ``app_label`` or ``app_label.model_name``
+            if '.' not in forvar:
+                app_label = forvar
+            else:
+                try:
+                    app_label, model_name = forvar.split('.')
+                except ValueError:
+                    msg = "%s isn't a valid value for the 'for' clause of tag %s" % forvar, tag_name
+                    raise template.TemplateSyntaxError(msg)
+        else: 
+            # since ``for`` clause's argument isn't quoted, we intepret it as a context variable 
+            # holding a QuerySet
+            content_qs_var = forvar         
+    
+    if limitvar:
+        try:
+            limit = int(limitvar)
+            if limit <= 0: raise ValueError 
+        except ValueError:
+            msg = "%s isn't a valid value for the 'limit' clause of tag %s" % limitvar, tag_name
+            raise template.TemplateSyntaxError(msg)
 
-def include_taglist(forvar=None):
-    return {'forvar': forvar}
-  
-register.inclusion_tag('taggit_templatetags/taglist_include.html')(include_taglist)
-register.inclusion_tag('taggit_templatetags/tagcloud_include.html')(include_tagcloud)
+    return TagListNode(asvar, app_label, model_name, content_qs_var, limit)
+             
+
+class TagListNode(template.Node):
+    def __init__(self, asvar, app_label, model_name, content_qs_var, limit):
+        self.asvar = asvar
+        self.app_label = app_label and app_label.lower() or None
+        self.model_name = model_name and model_name.lower() or None
+        self.content_qs_var = content_qs_var
+        self.limit = limit
+    
+    def render(self, context):
+        try:
+            # Django model representing tags
+            tag_model = get_model(*TAG_MODEL.split('.'))
+            # Django model representing tagged items (w.r.t. the tagging model in use)
+            through_model = get_model(*TAGGED_ITEM_MODEL.split('.'))
+            through_opts = through_model._meta
+            
+            if self.content_qs_var:
+                    self.content_qs = template.Variable(self.content_qs_var).resolve(context)
+            
+            # determine what kind of tagging model is in use
+            if is_generic_tagging(through_model):
+                ##-------- generic tagging model --------##
+                ## the ``QuerySet`` -- of tags -- to be annotated
+                tag_qs = tag_model.objects.all()
+                ## the ``QuerySet`` -- of generic models -- the annotation is performed against 
+                # start with a QuerySet comprising every tagged item in the DB  
+                generic_qs = through_model.objects.all()
+                if self.app_label and self.model_name:
+                    # filter away tagged items that aren't instances of the given model
+                    ct = ContentType.objects.get(app_label=self.app_label, model=self.model_name)
+                    generic_qs = generic_qs.filter(content_type=ct)
+                elif self.app_label:
+                    # filter away tagged items not belonging to the given app
+                    generic_qs = generic_qs.filter(content_type__app_label=self.app_label)
+                # restrict annotation to a subset of tagged content objects
+                if hasattr(self, 'content_qs'):
+                    generic_qs = generic_qs.filter(content_object__in=self.content_qs)
+                annotated_tag_qs = generic_annotate(tag_qs, through_model.content_object, Count('pk'), generic_qs, desc=True, alias='count')               
+            else:       
+                ##------- model-specific tagging model ---------##
+                content_model = through_opts.get_field_by_name('content_object')[0].rel.to
+                if self.app_label:
+                    if self.app_label != content_model._meta.app_label.lower():
+                        # wrong app label !
+                        return ''
+                    if self.model_name:
+                        if self.model_name != content_model._meta.object_name.lower():
+                            # wrong model name !
+                            return ''
+                # start with a QuerySet comprising every tag in the DB
+                qs = tag_model.objects.all()
+                if self.content_qs_var:
+                    content_qs = template.Variable(self.content_qs_var).resolve(context)
+                    if isinstance(content_qs, QuerySet) and (content_qs.model == content_model): # sanity check
+                        lookup_field = '%s__content_object__in' % through_opts.object_name.lower()
+                        lookup_args = {lookup_field: content_qs}
+                        qs = qs.filter(**lookup_args)  
+                    else:
+                        # invalid QuerySet
+                        return ''
+                aggregate_field = through_opts.get_field_by_name('tag')[0].rel.relname
+                qs = qs.annotate(count=Count(aggregate_field))
+                if self.limit:
+                    qs=qs[:self.limit]
+                annotated_tag_qs = qs.order_by('-count')
+            context[self.asvar] = annotated_tag_qs
+        except: # fail silently on rendering
+            return ''
+
+@register.tag(name="get_tag_cloud")
+def do_get_tag_cloud(parser, token):
+    pass
+
+class TagCloudNode(template.Node):
+    def __init__(self):
+        pass
+    
+    def render(self, context):
+        return ''
